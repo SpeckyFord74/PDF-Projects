79adc9296c6b9733bdcb701f35e860fcc3599f73
Corey <107110478+SpeckyFord74@users.noreply.github.com>
2022-09-12 21:07:15 +0100

    Add files via upload

---
+++ Refactor/Refactor/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Refactor
+{
+    class System
+    {
+        static void Main(string[] args)
+        {
+            computeFactorials(1, 8);
+            Console.ReadLine();
+        }
+
+        static int Factorial(int Num)
+        {
+            int Result;
+            if(Num == 1)
+            {
+                Result = 1;
+            }
+            else
+            {
+                Result = (Factorial(Num - 1) * Num);
+            }
+            return Result;
+        }
+
+        static void computeFactorials(int Number, int Max)
+        {
+            while(Number <= Max)
+            {
+                Console.Write($"Factorial of {Number}:");
+                Console.WriteLine(Factorial(Number));
+                Number++;
+            }
+        }
+    }
+}
