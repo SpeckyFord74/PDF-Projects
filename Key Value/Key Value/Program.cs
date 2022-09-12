a0e96aee5ad78fd91ee228324df4732fc792b1b0
Corey <107110478+SpeckyFord74@users.noreply.github.com>
2022-09-12 21:04:21 +0100

    Add files via upload

---
+++ Key Value/Key Value/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Key_Value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, string> BookList = 
+                new Dictionary<string, string>();
+            BookList.Add("Stuart Yarnold", "Arduino");
+            BookList.Add("Nick Vandome", "Windows 10");
+            BookList.Add("Mike McGrath", "Java");
+
+            Console.WriteLine("Popular Titles...");
+
+            foreach (KeyValuePair<string, string> books in BookList)
+            {
+                Console.WriteLine($"Key: {books.Key}\tValue: {books.Value} in easy steps");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Manipulate/Manipulate/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Manipulate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Please enter text: ");
+            string Text = Console.ReadLine();
+
+            Console.Write($"\nThanks. You entered: {Text}");
+            Console.WriteLine($"\n\nText length: {Text.Length}");
+
+            Text = Text.Trim();
+            Console.Write($"\nTrimmed:\t{Text.Length}");
+            Console.WriteLine($"\tText length: {Text.Length}");
+
+            string Upper = Text.ToUpper();
+            Console.WriteLine($"\nUpper case:\t{Upper}");
+
+            string Lower = Text.ToLower();
+            Console.WriteLine($"\nLower case:\t{Lower}");
+
+            Upper = Upper.PadLeft(40);
+            Lower = Lower.PadRight(40, '#');
+            Text = Text.PadLeft(30, '*').PadRight(40, '#');
+
+            Console.WriteLine($"\nPadded left:\t{Upper}");
+            Console.WriteLine($"Padded right:\t{Lower}");
+            Console.WriteLine($"Padded both:\t{Text}");
+
+            Console.Write($"\nTrimmed start:\t{Upper.TrimStart()}");
+            Console.Write($"Trimmed end:\t{Text.TrimEnd()}");
+
+            Console.ReadLine();
+        }
+    }
+}
